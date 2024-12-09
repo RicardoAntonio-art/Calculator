b2e4973441843a5901e939c0d8b9ebcf9542dae1
Ricardo Antonio-arte <ricardoazm23@gmail.com>
2024-12-09 17:18:38 -0600

    Reestructuracion del codigo, se implementa el uso de objetos

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-let digits1 = [];
-let digits2 = [];
-let operator;
+let digitos = {
+    posicion1: undefined,
+    posicion2: undefined,
+};
 
-const digits = document.querySelectorAll("#digit");
+let calculadora = {
+    operaciones: undefined,
+};
+const botones = document.querySelectorAll("#digit");
+const operadores = document.querySelectorAll("#operator")
+
+
+operadores.forEach( (operadores) => { 
+    operadores.addEventListener("click", () =>{ 
+        calculadora.operaciones = operadores.textContent;
+        console.log(calculadora.operaciones);
+    });
+});
+
+
+botones.forEach((botones) =>{ 
+    botones.addEventListener("click",()=>{ 
+        digitos.posicion1 = botones.textContent;
+        console.log(digitos.posicion1);
+    });
+});
+
+/*
 const operators = document.querySelectorAll("#operator");
+const pantalla = document.querySelector("#pantalla");
+const resultado = document.querySelector("#equals");
+const digitsPAN = document.createElement("p");
 
 
 operators.forEach((operator) =>{ 
     operator.addEventListener("click", () => {
-        operator = operator.textContent;
+        operator = operator.textContent;         
         console.log(operator);
     });
 });
+*/
 
-digits.forEach((digits) =>{ 
-    digits.addEventListener("click",()=>{ 
-    let digits1 = digits.textContent;  
-    console.log(digits1);
-    });
-});
+
+function suma (){ 
+   return digit1+digit2; 
+}
+
 
 
 function operate(digits1,digits22,operator) { 
